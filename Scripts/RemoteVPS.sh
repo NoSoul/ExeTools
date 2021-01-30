8d19db26b1727be29032a13fad98f9c0330145b0
NoSoul <NoSoul.Love@GMail.com>
2021-01-30 22:05:15 +0800

    update

--- Scripts/RemoteVPS.sh
+++ Scripts/RemoteVPS.sh
@@ -3,10 +3,10 @@ if [ $# -ne 1 ];then
     echo "Usage: $0 p/l"
     exit 1;
 fi
-ip=207.148.22.201
+ip=104.207.152.104
 port=22
 if [ $1 = 'p' ];then
-    ssh -D7070 -p$port -N root@$ip
+    ssh -4 -D7070 -p$port -N root@$ip
     exit 0;
 elif [ $1 = 'l' ];then
     ssh -p$port root@$ip
--- Scripts/Rescan.sh
+++ Scripts/Rescan.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 pciSlot=$(lspci  | grep Wireless | awk '{print $1}')
-echo 1 > /sys/devices/pci0000:00/0000:$pciSlot/remove
+echo 1 > /sys/bus/pci/devices/0000:$pciSlot/reset
+echo 1 > /sys/bus/pci/devices/0000:$pciSlot/remove
 echo 1 > /sys/bus/pci/rescan
